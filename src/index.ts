cbe71ef3f86a5ce4be0eb71d26774580fc24639e
xoma_star <sasha.sirotyin09@gmail.com>
2023-01-15 19:20:25 +0500

    a lot of changes

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import startExpress from "./api";
 
 const token = '5828331861:AAHs0VGlM4BJhJbWF9lVQxEeL8j187m_QcQ'
 
-const bot = new TelegramBot(token, {polling: true})
-
-bot.setMyCommands([
-    {command: '/start', description: 'Приветственное сообщение'},
-    {command: '/first_steps', description: 'Первые шаги'},
-    {command: '/first_steps_galaxy', description: 'Первые шаги: устройство галактики'}
-])
-
-addListeners(bot)
+// const bot = new TelegramBot(token, {polling: true})
+//
+// bot.setMyCommands([
+//     {command: '/start', description: 'Приветственное сообщение'},
+//     {command: '/first_steps', description: 'Первые шаги'},
+//     {command: '/first_steps_galaxy', description: 'Первые шаги: устройство галактики'}
+// ])
+//
+// addListeners(bot)
 startExpress()
