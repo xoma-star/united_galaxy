8e4556fb010e39c0be85f73f1014b609121b3fae
xoma_star <sasha.sirotyin09@gmail.com>
2023-01-15 19:19:37 +0500

    a lot of changes

--- src/keyboards/default.keyboard.ts
+++ src/keyboards/default.keyboard.ts
@@ -3,7 +3,11 @@ import {KeyboardButton} from "node-telegram-bot-api";
 const DefaultKeyboard = (coordinates?: string): KeyboardButton[][] => [
     [{text: 'Биржа'}, {text: 'Корабль'}, {text: 'Система'}],
     [{text: 'Колонии'}, {text: 'Компания'}],
-    [{text: 'Карта', web_app: {url: `https://united-galaxy.vercel.app` + (coordinates ? `/?sector=${coordinates?.split(':').map(x => x.slice(0, 3)).join(':')}` : '')}}]
+    [{text: 'Карта', web_app: {url:
+                `https://united-galaxy.vercel.app` +
+                (coordinates ? `/?sector=${coordinates?.split(':').map(x => x.slice(0, 3)).join(':')}` : '') +
+                (coordinates ? `&coordinates=${coordinates}` : '')
+    }}]
 ]
 
 export default DefaultKeyboard
